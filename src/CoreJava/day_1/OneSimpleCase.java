efde2899e462eeb095f97f6781e58c53b22bc308
657994434 <657994434@qq.com>
2016-11-17 19:54:43 +0800

    第一个Java小程序

---
+++ src/CoreJava/day_1/OneSimpleCase.java
@@ -0,0 +1,11 @@
+package CoreJava.day_1;
+
+/**
+ * @author 李智
+ * @date 2016/11/17
+ */
+public class OneSimpleCase {
+    public static void main(String[] args) {
+        System.out.println("Java核心技术I!");
+    }
+}
