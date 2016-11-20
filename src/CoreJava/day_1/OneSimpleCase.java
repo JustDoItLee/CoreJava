e426281ed521a10fd6fa47b0771be7828d401e96
657994434 <657994434@qq.com>
2016-11-20 19:25:01 +0800

    基本类型

--- src/CoreJava/day_1/OneSimpleCase.java
+++ src/CoreJava/day_1/OneSimpleCase.java
@@ -7,5 +7,7 @@ package CoreJava.day_1;
 public class OneSimpleCase {
     public static void main(String[] args) {
         System.out.println("Java核心技术I!");
+        System.out.println(2.0-1.1);
+        System.out.println((byte)300);//强转超出范围
     }
 }
