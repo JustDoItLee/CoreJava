1e49c6ef5231ac927b9b9a5b27cc1e3ca621206b
657994434 <657994434@qq.com>
2016-11-20 22:19:44 +0800

    字符串

--- src/CoreJava/day_1/OneSimpleCase.java
+++ src/CoreJava/day_1/OneSimpleCase.java
@@ -7,7 +7,20 @@ package CoreJava.day_1;
 public class OneSimpleCase {
     public static void main(String[] args) {
         System.out.println("Java核心技术I!");
-        System.out.println(2.0-1.1);
-        System.out.println((byte)300);//强转超出范围
+        System.out.println(2.0 - 1.1);
+        System.out.println((byte) 300);//强转超出范
+        String name = "RicardoLee";
+        System.out.println("the name is :" + name);
+        String s = "hello";
+        System.out.println(s == "hello");
+        System.out.println(s.substring(0, 3) == "hel");
+        //代码点
+        System.out.println("__________________________");
+        System.out.println(s.charAt(1));
+        System.out.println(s.codePointCount(0, s.length()));
+        for (int i = 0; i < s.length(); i++) {
+            int index = s.offsetByCodePoints(0, i);
+            System.out.println(s.codePointAt(index));
+        }
     }
 }
