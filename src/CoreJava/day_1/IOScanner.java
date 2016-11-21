a348c2859ebf1de261d01f62b903703e461d4b87
657994434 <657994434@qq.com>
2016-11-21 23:15:14 +0800

    基本操作

---
+++ src/CoreJava/day_1/IOScanner.java
@@ -0,0 +1,32 @@
+package CoreJava.day_1;
+
+import java.io.Console;
+import java.util.Date;
+import java.util.Scanner;
+
+/**
+ * @author 李智
+ * @date 2016/11/21
+ */
+public class IOScanner {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("input your name;");
+//        String name = in.nextLine();//一行
+//        String name = in.next();//一个单词 以空格为分隔符
+//        System.out.println(name);
+//        Java.io.Console只能用在标准输入、输出流未被重定向的原始控制台中使用，在 Eclipse 或者其他 IDE 的控制台是用不了的。
+//        Console console = System.console();
+//        String username = console.readLine("what's your username?");
+//        char[] password = console.readPassword("password");
+        double x = 10000.0/3.0;
+        System.out.println(x);
+        System.out.printf("%8.2f",x);
+        System.out.printf("%,.2f\n",10000.0/3.0);
+        System.out.printf("%tc\n",new Date());//时间格式
+        System.out.println(System.getProperty("OneSimpleCase.class"));
+        for (int i = 10;i>0;i--){
+            System.out.println("counting down----"+i);
+        }
+    }
+}
