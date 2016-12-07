f86e2b94e245a544711546343142981cc296feee
657994434 <657994434@qq.com>
2016-12-07 22:31:53 +0800

    匿名内部类

---
+++ src/CoreJava/day_3/AnnoymousInnerClassTest.java
@@ -0,0 +1,36 @@
+package CoreJava.day_3;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Date;
+
+/**
+ * @author 李智
+ * @date 2016/12/7
+ *
+ * 匿名内部类
+ */
+public class AnnoymousInnerClassTest {
+    public static void main(String[] args) {
+
+    }
+}
+
+class TalkingClock2 {
+    public void start(int interval, final boolean beep) {
+        ActionListener listener = new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Date now = new Date();
+                System.out.println("now :" + now);
+                if (beep) {
+                    Toolkit.getDefaultToolkit().beep();
+                }
+            }
+        };
+        Timer t = new Timer(interval, listener);
+        t.start();
+    }
+}
