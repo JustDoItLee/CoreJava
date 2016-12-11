aea86fe5699cb010a55da611e844928e4f42bde2
657994434 <657994434@qq.com>
2016-12-11 21:37:49 +0800

    简单的泛型测试

---
+++ src/CoreJava/day_5/Pair.java
@@ -0,0 +1,38 @@
+package CoreJava.day_5;
+
+/**
+ * @author 李智
+ * @date 2016/12/11
+ */
+public class Pair<T> {
+    private T first;
+    private T second;
+
+    public Pair() {
+        first = null;
+        second = null;
+    }
+
+    ;
+
+    public Pair(T first, T second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public T getFirst() {
+        return first;
+    }
+
+    public void setFirst(T first) {
+        this.first = first;
+    }
+
+    public T getSecond() {
+        return second;
+    }
+
+    public void setSecond(T second) {
+        this.second = second;
+    }
+}
