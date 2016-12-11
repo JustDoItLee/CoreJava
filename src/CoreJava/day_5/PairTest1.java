213cb863f5a675986c2f5c4ce25604c773a83823
657994434 <657994434@qq.com>
2016-12-11 22:42:44 +0800

    遍历String数组取出最大值最小值

--- src/CoreJava/day_5/PairTest1.java
+++ src/CoreJava/day_5/PairTest1.java
@@ -25,4 +25,14 @@ class ArrayLag {
         }
         return new Pair<>(min, max);
     }
+
+    //泛型作用在方法
+    public static <T extends Comparable> T min(T[] a) {
+        if (a == null || a.length == 0) return null;
+        T smallest = a[0];
+        for (int i = 1; i < a.length; i++) {
+            if (smallest.compareTo(a[i]) > 0) smallest = a[i];
+        }
+        return smallest;
+    }
 }
