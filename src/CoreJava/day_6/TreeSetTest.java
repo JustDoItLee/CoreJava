83d5c7b30626df3dab0245ad199ab72529d2d6e8
657994434 <657994434@qq.com>
2016-12-23 23:26:38 +0800

    TreeSet

--- src/CoreJava/day_6/TreeSetTest.java
+++ src/CoreJava/day_6/TreeSetTest.java
@@ -11,9 +11,9 @@ import java.util.TreeSet;
 public class TreeSetTest {
     public static void main(String[] args) {
         SortedSet<Item> parts = new TreeSet<>();
-        parts.add(new Item("Toaster",1234));
-        parts.add(new Item("Widget",4562));
-        parts.add(new Item("Modem",9912));
+        parts.add(new Item("Toaster", 1234));
+        parts.add(new Item("Widget", 4562));
+        parts.add(new Item("Modem", 9112));
         System.out.println(parts);
 
         SortedSet<Item> sortByDescription = new TreeSet<>(
