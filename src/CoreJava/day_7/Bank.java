6c64d82436033416dd337e652a2fb204aa799a73
657994434 <657994434@qq.com>
2016-12-27 22:47:37 +0800

    经典多线程银行转账(synchronized)

--- src/CoreJava/day_7/Bank.java
+++ src/CoreJava/day_7/Bank.java
@@ -22,7 +22,7 @@ public class Bank {
         sufficientFunds = bankLock.newCondition();
     }
 
-    public void transfer(int from, int to, double amount) throws InterruptedException {
+    /*public void transfer(int from, int to, double amount) throws InterruptedException {
         bankLock.lock();
         try {
             while (accounts[from] < amount) {
@@ -39,19 +39,39 @@ public class Bank {
         }
 
 
+    } */
+    public synchronized void transfer(int from, int to, double amount) throws InterruptedException {
+        while (accounts[from] < amount) {
+            wait();
+        }
+        System.out.print(Thread.currentThread());
+        accounts[from] -= amount;
+        System.out.printf("%10.2f from %d to %d", amount, from, to);
+        accounts[to] += amount;
+        System.out.printf("Total Balance:%10.2f%n", getTotalBalance());
+        notifyAll();
     }
 
-    public double getTotalBalance() {
-        bankLock.lock();
-        try {
-            double sum = 0;
-            for (double a : accounts) {
-                sum += a;
-            }
-            return sum;
-        } finally {
-            bankLock.unlock();
+
+    /* public double getTotalBalance() {
+         bankLock.lock();
+         try {
+             double sum = 0;
+             for (double a : accounts) {
+                 sum += a;
+             }
+             return sum;
+         } finally {
+             bankLock.unlock();
+         }
+
+     }*/
+    public synchronized double getTotalBalance() {
+        double sum = 0;
+        for (double a : accounts) {
+            sum += a;
         }
+        return sum;
 
     }
 
