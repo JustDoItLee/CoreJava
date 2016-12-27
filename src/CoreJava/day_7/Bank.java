025de3e90e084b72d807c7a04e6f09f3df99615e
657994434 <657994434@qq.com>
2016-12-27 19:46:36 +0800

    经典多线程银行转账(加锁)

--- src/CoreJava/day_7/Bank.java
+++ src/CoreJava/day_7/Bank.java
@@ -1,34 +1,58 @@
 package CoreJava.day_7;
 
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 /**
  * @author 李智
  * @date 2016/12/26
  */
 public class Bank {
     private final double[] accounts;
+    private Lock bankLock;
+    private Condition sufficientFunds;
 
     public Bank(int n, double initialBalance) {
         accounts = new double[n];
         for (int i = 0; i < accounts.length; i++) {
             accounts[i] = initialBalance;
         }
+        bankLock = new ReentrantLock();
+        sufficientFunds = bankLock.newCondition();
     }
 
-    public void transfer(int from, int to, double amount) {
-        if (accounts[from] < amount) return;
-        System.out.print(Thread.currentThread());
-        accounts[from] -= amount;
-        System.out.printf("%10.2f from %d to %d", amount, from, to);
-        accounts[to] += amount;
-        System.out.printf("Total Balance:%10.2f%n", getTotalBalance());
+    public void transfer(int from, int to, double amount) throws InterruptedException {
+        bankLock.lock();
+        try {
+            while (accounts[from] < amount) {
+                sufficientFunds.await();
+            }
+            System.out.print(Thread.currentThread());
+            accounts[from] -= amount;
+            System.out.printf("%10.2f from %d to %d", amount, from, to);
+            accounts[to] += amount;
+            System.out.printf("Total Balance:%10.2f%n", getTotalBalance());
+            sufficientFunds.signalAll();
+        } finally {
+            bankLock.unlock();
+        }
+
+
     }
 
     public double getTotalBalance() {
-        double sum = 0;
-        for (double a : accounts) {
-            sum += a;
+        bankLock.lock();
+        try {
+            double sum = 0;
+            for (double a : accounts) {
+                sum += a;
+            }
+            return sum;
+        } finally {
+            bankLock.unlock();
         }
-        return sum;
+
     }
 
     public int size() {
